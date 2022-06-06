a92c7a2692f4eaa969da6f3608871105ea6519e7
Puskar Adhikari <puskar.ad@gmail.com>
2022-06-06 17:44:01 +0545

    example template to test package locally

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SayHello from 'react-custome-hooks'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <SayHello name='Prateek' />
+    </div>
+  )
+}
+
+export default App
