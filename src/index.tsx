7bf850580f118acc629c16c55a811b40058609f4
Puskar Adhikari <puskar.ad@gmail.com>
2022-06-08 15:55:23 +0545

    feat: get scroll position hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,2 @@
-import React from 'react'
-
-const SayHello = ({ name }: { name: string }): JSX.Element => (
-  <div>Hey {name}, say hello to TypeScript.</div>
-)
-
-export default SayHello
+export * from './hooks'
+export * from './hooks/useLocalStorage'
