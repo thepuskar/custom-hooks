13fc7af6ff1e5e6d41c98697fb0d827c3879f2eb
Puskar Adhikari <puskar.ad@gmail.com>
2022-12-18 15:23:02 +0545

    created a file based routing

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createRoot } from 'react-dom/client'
+
+import { Routes } from './routes'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const container = document.getElementById('root')!
+createRoot(container).render(<Routes />)
---
+++ example/src/routes.tsx
@@ -0,0 +1,62 @@
+import { Fragment, lazy, Suspense } from 'react'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+import type {
+  ActionFunction,
+  RouteObject,
+  LoaderFunction
+} from 'react-router-dom'
+
+import { generatePreservedRoutes, generateRegularRoutes } from './core'
+
+type Element = () => JSX.Element
+type Module = {
+  default: Element
+  Loader: LoaderFunction
+  Action: ActionFunction
+  ErrorElement: Element
+}
+
+const PRESERVED = import.meta.glob<Module>('/src/pages/(_app|404).{jsx,tsx}', {
+  eager: true
+})
+const ROUTES = import.meta.glob<Module>([
+  '/src/pages/**/[\\w[]*.{jsx,tsx}',
+  '!**/(_app|404).*'
+])
+
+const preservedRoutes = generatePreservedRoutes<Element>(PRESERVED)
+
+const regularRoutes = generateRegularRoutes<RouteObject, () => Promise<Module>>(
+  ROUTES,
+  (module, key) => {
+    const Element = lazy(module)
+    const ErrorElement = lazy(() =>
+      module().then((module) => ({ default: module.ErrorElement || null }))
+    )
+    const index = /(?<!pages\/)index\.(jsx|tsx)$/.test(key)
+      ? { index: true }
+      : {}
+
+    return {
+      ...index,
+      element: <Suspense fallback={null} children={<Element />} />,
+      loader: (...args) =>
+        module().then((mod) => mod?.Loader?.(...args) || null),
+      action: (...args) =>
+        module().then((mod) => mod?.Action?.(...args) || null),
+      errorElement: <Suspense fallback={null} children={<ErrorElement />} />
+    }
+  }
+)
+
+const App = preservedRoutes?.['_app'] || Fragment
+const NotFound = preservedRoutes?.['404'] || Fragment
+
+export const routes = [...regularRoutes, { path: '*', element: <NotFound /> }]
+const router = createBrowserRouter([
+  { element: <App children={<Outlet />} />, children: routes }
+])
+
+export const Routes = () => {
+  return <RouterProvider router={router} />
+}
